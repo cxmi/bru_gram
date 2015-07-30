7b54c20e21db547770e1c8572520da9d7db493cc
cxmi <christinexmi@gmail.com>
2015-07-30 15:31:09 +0200

    added bootstrap theme but still debugging

--- app/helpers/authorization_helper.rb
+++ app/helpers/authorization_helper.rb
@@ -8,4 +8,15 @@ module AuthorizationHelper
 		comment.user == current_user
 	end
 
+	def photo_with_hashtag_links(photo)
+		photo.caption.gsub(/#\w+/) do |tag|
+			link_to tag, hashtag_path(Hashtag.find_by(tag: tag))
+		end.html_safe
+	end
+
+# 	in index html:
+# 	<% @hashtag.photos.each do |photo| %>
+# 	<%= photo_with_hashtag_links(photo) %><br>
+# <% end %>
+
 end
--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -2,12 +2,21 @@ class Hashtag < ActiveRecord::Base
 
 	has_and_belongs_to_many :photos
 
-	def self.parse(caption)
+	# def self.parse(caption)
 
-		tags = []
-		caption.scan(/#\w+/).each do |tag|
+	# 	tags = []
+	# 	caption.scan(/#\w+/).each do |tag|
+	# 		tags << Hashtag.find_or_initialize_by(tag: tag)
+	# 	end
+	# 	tags
+	# end
+
+
+		def self.parse(caption)
+		caption.scan(/#\w+/).map do |tag|
 			tags << Hashtag.find_or_initialize_by(tag: tag)
 		end
-		tags
+		
 	end
+
 end
